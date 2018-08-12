5b181f98958f383ccf86551f29779ccc34d90224
Faryyaz <dilfary@hotmail.com>
2018-08-12 12:00:53 +0400

    Added menu and hero components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule
