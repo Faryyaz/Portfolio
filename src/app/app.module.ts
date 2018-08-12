ebb20096e6b8965cc14534d837443ecde217309a
Faryyaz <dilfary@hotmail.com>
2018-08-12 17:50:45 +0400

    Update to component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { HeroComponent } from './hero/hero.component';
+import { ObjectivesComponent } from './objectives/objectives.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    HeroComponent
+    HeroComponent,
+    ObjectivesComponent,
+    WorkExperienceComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/objectives/objectives.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-objectives',
+    templateUrl: './objectives.component.html',
+    styleUrls: ['./objectives.component.scss']
+})
+export class ObjectivesComponent{
+}
