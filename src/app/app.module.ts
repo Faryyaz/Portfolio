001d94ec2393fb77deaecb5521f1f4e120cac817
Faryyaz <dilfary@hotmail.com>
2018-08-23 11:10:28 +0400

    Component moved to home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HeroComponent } from './hero/hero.component';
-import { ObjectivesComponent } from './objectives/objectives.component';
-import { WorkExperienceComponent } from './work-experience/work-experience.component';
-import { SkillsComponent } from './skills/skills.component';
-import { SkillComponent } from './skills/skill/skill.component';
-import { OtherComponent } from './skills/other/other.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { PersonalComponent } from './projects/personal/personal.component';
-import { ProfessionalComponent } from './projects/professional/professional.component';
-import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { HeroComponent } from './home/hero/hero.component';
+import { ObjectivesComponent } from './home/objectives/objectives.component';
+import { WorkExperienceComponent } from './home/work-experience/work-experience.component';
+import { SkillsComponent } from './home/skills/skills.component';
+import { SkillComponent } from './home/skills/skill/skill.component';
+import { OtherComponent } from './home/skills/other/other.component';
+import { ProjectsComponent } from './home/projects/projects.component';
+import { PersonalComponent } from './home/projects/personal/personal.component';
+import { ProfessionalComponent } from './home/projects/professional/professional.component';
+import { TestimonialsComponent } from './home/testimonials/testimonials.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 
