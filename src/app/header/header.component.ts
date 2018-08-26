10900c29f0ad971fe6214e6a7b3f415a2882b0a1
Faryyaz <dilfary@hotmail.com>
2018-08-26 21:39:39 +0400

    Scroll effect and fragments added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit} from '@angular/core';
-import { Router, NavigationEnd, Event } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, Event, ActivatedRoute } from '@angular/router';
 
-@Component ({
+@Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
@@ -11,16 +11,18 @@ export class HeaderComponent implements OnInit {
     isDropDownActive = false;
     isHomeActive = false;
 
-    constructor(private router: Router){}
+    constructor(private router: Router, private route: ActivatedRoute) { }
 
     ngOnInit() {
         this.router.events.subscribe(
             (event: Event) => {
-                if(event instanceof NavigationEnd) {
-                    if(this.router.url === '/') {
+                if (event instanceof NavigationEnd) {
+                    let url = this.router.url.replace(/#.+/g, '');
+                    if (url === '/') {
                         this.isHomeActive = true;
                     } else {
                         this.isHomeActive = false;
+                        this.isDropDownActive = false;
                     }
                 }
             }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.fragment.subscribe(f => {
+      const element = document.getElementById(f);
+      const body = document.getElementsByClassName('route-container');
+      if (element) {
+        setTimeout(() => {
+          let offset = element.offsetTop - element.scrollTop - 120; // xx = any extra distance from top ex. 60
+          window.scrollBy({top: offset, left: 0, behavior: 'smooth'});
+        }, 100);
+      }
+    });
   }
 
 }
