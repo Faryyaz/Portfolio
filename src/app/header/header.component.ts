5e2013b392db79b1a7072a6a66bc87c4b604dc73
Faryyaz <dilfary@hotmail.com>
2018-08-26 18:30:18 +0400

    Dropdown menu animation + active class for home url

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,35 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Router, NavigationEnd, Event } from '@angular/router';
 
 @Component ({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
     name = "FD";
+    isDropDownActive = false;
+    isHomeActive = false;
+
+    constructor(private router: Router){}
+
+    ngOnInit() {
+        this.router.events.subscribe(
+            (event: Event) => {
+                if(event instanceof NavigationEnd) {
+                    if(this.router.url === '/') {
+                        this.isHomeActive = true;
+                    } else {
+                        this.isHomeActive = false;
+                    }
+                }
+            }
+        );
+    }
+
+    openDropDownMenu() {
+        this.isDropDownActive = !this.isDropDownActive;
+    }
+
+
 }
