25a48be9a0041ade61537ff96a07b1fd6005238c
Faryyaz <dilfary@hotmail.com>
2019-05-16 07:33:49 +0400

    Email function + view and comments added

--- Backend/app/Http/Controllers/Controller.php
+++ Backend/app/Http/Controllers/Controller.php
@@ -6,11 +6,17 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+    /**
+     * Generate work experience response
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function workExperience()
     {
         $workExperience = \App\DataFormatter::getWorkExperience();
@@ -20,6 +26,11 @@ class Controller extends BaseController
         ], 200);
     }
 
+    /**
+     * Generate work timeline response
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function workTimeLine()
     {
         $workTimeLine = \App\DataFormatter::getWorkTimeline();
@@ -29,6 +40,11 @@ class Controller extends BaseController
         ], 200);
     }
 
+    /**
+     * Generate project response
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function projects()
     {
         $projects = \App\DataFormatter::getProjects();
@@ -38,6 +54,11 @@ class Controller extends BaseController
         ], 200);
     }
 
+    /**
+     * Generate main skills response
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function mainSkills()
     {
         $skills = \App\DataFormatter::getMainSkills();
@@ -47,6 +68,11 @@ class Controller extends BaseController
         ], 200);
     }
 
+    /**
+     * Generate other skills response
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function otherSkills()
     {
         $others = \App\DataFormatter::getOtherSkills();
@@ -56,6 +82,11 @@ class Controller extends BaseController
         ], 200);
     }
 
+    /**
+     * Generate testimonials response
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function testimonials()
     {
         $testimonials = \App\DataFormatter::getTestimonials();
@@ -63,5 +94,47 @@ class Controller extends BaseController
         return response()->json([
             'testimonials' => $testimonials,
         ], 200);
+    }
+
+    /**
+     * Send an email from contact me page
+     *
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sendQuotation(Request $request)
+    {
+
+        $this->validate($request, [
+            'name'     => 'required|max:255',
+            'email'    => 'required|email',
+            'company' => 'required',
+            'message'  => 'required',
+        ]);
+
+        \Mail::send('email', $request->all(), function ($message) {
+
+            $message->subject("Portfolio: Email from company");
+
+            $message->to(env('MY_MAIL_ADDRESS'));
+
+        });
+
+        // send failure response
+        if (\Mail::failures()) {
+            return response()->json([
+                'status'  => 'failed',
+                'message' => 'Whooops, an error occured while sending the email. Please try again later.',
+            ]);
+        }
+
+        // send success response
+        return response()->json(
+            [
+                'status'  => 'sent',
+                'message' => 'Thank you. Your request has been sent and I will get back to you soon.'
+            ]);
+
     }
 }
---
+++ Backend/resources/views/email.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Send Email</title>
+    </head>
+    <body>
+        <p>Name : {{ $name }}</p>
+        <p>Email: {{ $email }}</p>
+        <p>Company : {{ $company }}</p>
+        <br>
+        <p>{{ $message }}</p>
+    </body>
+</html>
