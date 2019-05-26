bc1dcbb15e7af1eb3bea4a9539b86727cc44f88a
Faryyaz <dilfary@hotmail.com>
2019-05-26 09:52:31 +0400

    Migration updates

--- Backend/database/migrations/2019_05_09_025114_create_projects_table.php
+++ Backend/database/migrations/2019_05_09_025114_create_projects_table.php
@@ -33,7 +33,7 @@ class CreateProjectsTable extends Migration
                 'title' => 'Dashboard',
                 'description' => 'Graphical representation of KPI data via a web based dashboard developed. An ETL process where data were extracted from XML files present on different network devices.',
                 'graphic_type' => 'icon',
-                'graphic_data' => 'fas fa-chart-line',
+                'graphic_data' => 'fas fa-chart-pie',
                 'link' => '',
                 'company' => 'Emtel Ltd',
                 'type' => 'professional'
@@ -51,7 +51,7 @@ class CreateProjectsTable extends Migration
                 'title' => 'Analytics',
                 'description' => 'Graphical representation of more in-depth KPI. ETL process whereby data are aggregated accordingly using their respective formulas.',
                 'graphic_type' => 'icon',
-                'graphic_data' => 'fas fa-map',
+                'graphic_data' => 'fas fa-chart-line',
                 'link' => '',
                 'company' => 'Emtel Ltd',
                 'type' => 'professional'
@@ -60,20 +60,84 @@ class CreateProjectsTable extends Migration
                 'title' => 'Proxy',
                 'description' => 'Graphical representation of domain/sub-domain visited and filtered accordingly. System developed using squid by filtering traffic.',
                 'graphic_type' => 'icon',
-                'graphic_data' => 'fas fa-map',
+                'graphic_data' => 'fas fa-sitemap',
                 'link' => '',
                 'company' => 'Stella Telecom',
                 'type' => 'professional'
             ],
             [
                 'title' => 'Internet Eligibility Test',
-                'description' => 'Web interface whereby user can test if he/she is eligible for internet. API is consumed for every web platform (e.g. Wordpress, etc...)',
+                'description' => 'Web interface whereby user can test if he/she is eligible for internet. API consumption for every web platform (e.g. Wordpress, etc...)',
                 'graphic_type' => 'icon',
-                'graphic_data' => 'fas fa-map',
+                'graphic_data' => 'fas fa-cogs',
                 'link' => '',
                 'company' => 'Stella Telecom',
                 'type' => 'professional'
             ],
+            [
+                'title' => 'Client online platform',
+                'description' => 'Web interface whereby client can configure products/services purchased. Developed using zend framework.',
+                'graphic_type' => 'icon',
+                'graphic_data' => 'fas fa-wrench',
+                'link' => '',
+                'company' => 'Stella Telecom',
+                'type' => 'professional'
+            ],
+            [
+                'title' => 'Online shop',
+                'description' => 'Allows customers to buy product online. Frontend development of the project based on prestashop.',
+                'graphic_type' => 'icon',
+                'graphic_data' => 'fas fa-store',
+                'link' => '',
+                'company' => 'Stella Telecom',
+                'type' => 'professional'
+            ],
+            [
+                'title' => 'Corporate website',
+                'description' => 'Use to provide background info about the company. Developed using Wordpress. Development involving API consumption, plugins, pages and web forms.',
+                'graphic_type' => 'icon',
+                'graphic_data' => 'fas fa-globe',
+                'link' => '',
+                'company' => 'Stella Telecom',
+                'type' => 'professional'
+            ],
+            [
+                'title' => 'IT Service Management tool (ITSM)',
+                'description' => 'Tool use to improve business use of information technology. Business development within ITSM tool (ServiceNow) using JS (AngularJS & Prototype)',
+                'graphic_type' => 'icon',
+                'graphic_data' => 'fas fa-project-diagram',
+                'link' => '',
+                'company' => 'Orange Business Services',
+                'type' => 'professional'
+            ],
+            [
+                'title' => 'Reporting tool',
+                'description' => 'Daily reports generated about server status. Developed using Laravel and ServiceNow API.',
+                'graphic_type' => 'icon',
+                'graphic_data' => 'fas fa-clipboard-list',
+                'link' => '',
+                'company' => 'Orange Business Services',
+                'type' => 'professional'
+            ],
+            [
+                'title' => 'Customer package system',
+                'description' => 'Web platform that allows customer to buy package delivery/transport services. Frontend development of web interfaces provided by UI/UX team based on JQuery/HTML5/SASS/MDL framework.',
+                'graphic_type' => 'icon',
+                'graphic_data' => 'fas fa-truck-loading',
+                'link' => '',
+                'company' => 'Extension Interactive',
+                'type' => 'professional'
+            ],
+            [
+                'title' => 'Customer online shop',
+                'description' => 'Web platform allowing customers to subscribed to services/products involving pets. Frontend development within Symphony framework using JQuery/Twig/SASS.',
+                'graphic_type' => 'icon',
+                'graphic_data' => 'fas fa-store-alt',
+                'link' => '',
+                'company' => 'Extension Interactive',
+                'type' => 'professional'
+            ],
+
 
             //personal
             [
@@ -82,6 +146,16 @@ class CreateProjectsTable extends Migration
                 'graphic_type' => 'image',
                 'graphic_data' => '/assets/img/poke_master.png',
                 'link' => 'https://play.google.com/store/apps/details?id=com.flatxg.pokemaster',
+                'company' => 'Personal | Part time',
+                'type' => 'personal'
+            ],
+            //personal
+            [
+                'title' => 'Portfolio',
+                'description' => 'Part time development of portfolio containing a compilation of all projects involved. Project developed using MySQL/SASS/Bootstrap/Angular/TypeScript/Laravel',
+                'graphic_type' => 'icon',
+                'graphic_data' => 'fas fa-id-badge',
+                'link' => 'https://github.com/Faryyaz/Portfolio',
                 'company' => 'Personal | Part time',
                 'type' => 'personal'
             ],
